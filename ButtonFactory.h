14cae3e0f9d996e0a097c02f0b7fe22d9ba73f23
brethepanther12 <bmoleus.nn@gmail.com>
2025-03-21 00:53:24 -0400

    Created button methods for the binary functions, trig functions, and clear, delete, equals, and signage.

--- ButtonFactory.h
+++ ButtonFactory.h
@@ -1,5 +1,25 @@
 #pragma once
+#include <wx/wx.h>
+
 class ButtonFactory
-{
+{ public:
+	static wxButton* CreateButton(wxWindow* parent, int id, const wxString& label);
+
+	
+	static wxButton* CreateNumbersButton(wxWindow* parent, int id, const wxString& label);
+	static wxButton* CreateAddButton(wxWindow* parent, int id);
+	static wxButton* CreateSubtractButton(wxWindow* parent, int id);
+	static wxButton* CreateMultiplyButton(wxWindow* parent, int id);
+	static wxButton* CreateDivideButton(wxWindow* parent, int id);
+	static wxButton* CreateModuloButton(wxWindow* parent, int id);
+
+	static wxButton* CreateClearButton(wxWindow* parent, int id);
+	static wxButton* CreateDeleteButton(wxWindow* parent, int id);
+	static wxButton* CreateEqualsButton(wxWindow* parent, int id);
+	static wxButton* CreateSignageButton(wxWindow* parent, int id);
+
+	static wxButton* CreateSinButton(wxWindow* parent, int id);
+	static wxButton* CreateCosButton(wxWindow* parent, int id);
+	static wxButton* CreateTanButton(wxWindow* parent, int id);
 };
 
