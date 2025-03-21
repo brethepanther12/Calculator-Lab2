fd1be2cccec11d85a18ac953843b13b59290684e
brethepanther12 <bmoleus.nn@gmail.com>
2025-03-21 01:13:52 -0400

    Difined button declaration of ButtonFactory header file to the cpp file.

--- ButtonFactory.cpp
+++ ButtonFactory.cpp
@@ -1,1 +1,19 @@
 #include "ButtonFactory.h"
+wxButton* CreateButton(wxWindow* parent, int id, const wxString& label) {return new wxButton(parent, id, label);};
+
+
+wxButton* CreateNumbersButton(wxWindow* parent, int id, const wxString& label) { return CreateButton(parent, id, label);};
+wxButton* CreateAddButton(wxWindow* parent, int id) {return CreateButton(parent, id, "+");};
+wxButton* CreateSubtractButton(wxWindow* parent, int id) {return CreateButton(parent, id, "-");};
+wxButton* CreateMultiplyButton(wxWindow* parent, int id) { return CreateButton(parent, id, "*"); };
+wxButton* CreateDivideButton(wxWindow* parent, int id) { return CreateButton(parent, id, "/"); };
+wxButton* CreateModuloButton(wxWindow* parent, int id) { return CreateButton(parent, id, "%"); };
+
+wxButton* CreateClearButton(wxWindow* parent, int id) { return CreateButton(parent, id, "Clear"); };
+wxButton* CreateDeleteButton(wxWindow* parent, int id) { return CreateButton(parent, id, "Delete"); };
+wxButton* CreateEqualsButton(wxWindow* parent, int id) { return CreateButton(parent, id, "="); };
+wxButton* CreateSignageButton(wxWindow* parent, int id) { return CreateButton(parent, id, "+/-"); };
+
+wxButton* CreateSinButton(wxWindow* parent, int id) { return CreateButton(parent, id, "sin"); };
+wxButton* CreateCosButton(wxWindow* parent, int id) { return CreateButton(parent, id, "cos"); };
+wxButton* CreateTanButton(wxWindow* parent, int id) { return CreateButton(parent, id, "tan"); };
