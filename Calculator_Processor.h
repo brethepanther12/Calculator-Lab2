3d310397c56fc4602dc2df65d6735acf4db6358f
brethepanther12 <bmoleus.nn@gmail.com>
2025-03-21 23:01:07 -0400

    Declared CalculatoreProcessor methods, constructor and get method for instance of calculation.

--- Calculator_Processor.h
+++ Calculator_Processor.h
@@ -1,5 +1,17 @@
 #pragma once
+#include <string>
+#include <cmath>
+#include <stdexcept>
 class Calculator_Processor
 {
+private:
+	static Calculator_Processor* _calculation;
+
+	Calculator_Processor();
+
+public:
+	static Calculator_Processor* GetCalculation();
+	
+	double Calculate(const std::string& expression);
 };
 
