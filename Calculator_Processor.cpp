c19558dfa19d1670065dfd592800eb6bc3064520
brethepanther12 <bmoleus.nn@gmail.com>
2025-03-21 23:16:07 -0400

    Defined CalculatorProcessor  in cpp file, initializing constructor and  starting tokening and shunting yard methods.

--- Calculator_Processor.cpp
+++ Calculator_Processor.cpp
@@ -1,1 +1,17 @@
 #include "Calculator_Processor.h"
+Calculator_Processor* Calculator_Processor::_calculation= nullptr;
+
+Calculator_Processor::Calculator_Processor() {};
+
+Calculator_Processor* Calculator_Processor::GetCalculation() {
+	if (!_calculation) {
+		_calculation = new Calculator_Processor();
+	}
+	return _calculation;
+}
+
+double Calculator_Processor::Calculate(const std::string& expression) {
+
+	//Tokenization and yard here.
+	return 0.0;
+}
